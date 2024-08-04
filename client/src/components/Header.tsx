81744f225b9a2b3e87b45b201c39fae663dd2447
Inette <eviefryeog@gmail.com>
2024-08-04 15:40:58 +0300

    start page modified

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import logo from "../assets/calendar.png"
 
 const Header = () => {
 	return (
 		<div className="header_container">
-			<div className="header_info_container">FAQ</div>
-			<div className="header_auth_container">
-				<div className="header_auth_button">Log In</div>
-				<div className="header_auth_button">Sign Up</div>
+			<div className="header_logo_container">
+				<img src={logo} alt="logo"/>
+				Daily Planner
+			</div>
+			<div className="header_buttons_container">
+				<div className="header_button_start">
+					Get Started
+				</div>
 			</div>
 		</div>
 	);
--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -1,31 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import one_img from "../assets/1.png"
 
 const StartPage = () => {
 	return (
 		<div className="startpage_container">
 			<Header />
 			<div className="startpage_block_one">
-				<div className="one_square">
-					<div className="one_text">
-						Talent she for lively eat led sister. Entrance strongly packages she
-						out rendered get quitting denoting led. Dwelling confined improved
-						it he no doubtful raptures. Several carried through an of up attempt
-						gravity. Situation to be at offending elsewhere distrusts if.
-						Particular use for considered projection cultivated. Worth of do
-						doubt shall it their. Extensive existence up me contained he
-						pronounce do. Excellence inquietude assistance precaution any
-						impression man sufficient.
-						<br></br>
-						<br></br>Call park out she wife face mean. Invitation excellence
-						imprudence understood it continuing to. Ye show done an into.
-						Fifteen winding related may hearted colonel are way studied. County
-						suffer twenty or marked no moment in he. Meet shew or said like he.
-						Valley silent cannot things so remain oh to elinor. Far merits
-						season better tended any age hunted.
-					</div>
+				<div className="one_text_container">
+					<h1>Enjoy your time management with new daily planner</h1>
+					<br></br>
+					Many people nowadays struggle with remembering all the complex tasks
+					throughout the day. It's your way to minimize any mess in your daily
+					routine. Give it a try!
+					<div className="one_button">Get Started</div>
 				</div>
+				<img src={one_img} alt="one_img" className="one_img" />
 			</div>
 			<div className="startpage_block_two"></div>
 			<div className="startpage_block_three"></div>
